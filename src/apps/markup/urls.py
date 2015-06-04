c18d095d10df314f6aa2f7a3b3a8962a3711c268
poltor <v.poltoratsky@gmail.com>
2015-06-04 15:53:48 +0300

    Item page

--- src/apps/markup/urls.py
+++ src/apps/markup/urls.py
@@ -8,4 +8,7 @@ urlpatterns = patterns(
     url(r'^index/$', TemplateView.as_view(template_name='markup/index.html'), name="first"),
     url(r'^categories/$', TemplateView.as_view(template_name='markup/categories_list.html'), name="second"),
     url(r'^items/$', TemplateView.as_view(template_name='markup/items.html'), name="third"),
+    url(r'^item/$', TemplateView.as_view(template_name='markup/item.html'), name="ololo"),
+    url(r'^item-1/$', TemplateView.as_view(template_name='markup/item-1.html'), name="ololo1"),
+    url(r'^item-2/$', TemplateView.as_view(template_name='markup/item-2.html'), name="ololo2"),
 )
