6bfe906564a8581990c72a5192b450ba9829e367
Nikita Kolesnikov <koles.web@gmail.com>
2015-06-08 23:31:22 +0300

    staging settings up

--- src/settings/staging.py
+++ src/settings/staging.py
@@ -3,3 +3,6 @@ from .base import Base
 
 class Staging(Base):
     DEBUG = False
+    DEFAULT_HOST = 'http://alterprice.staging.corp.createdigital.me'
+    MEDIA_ROOT = '/media'
+    STATIC_ROOT = '/static'
