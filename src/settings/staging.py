3a93f202fe96595a57b6243c72487f3dba7cb857
Nikita Kolesnikov <koles.web@gmail.com>
2015-06-09 13:16:52 +0300

    ALLOWED_HOSTS added to staging settings file

--- src/settings/staging.py
+++ src/settings/staging.py
@@ -3,6 +3,7 @@ from .base import Base
 
 class Staging(Base):
     DEBUG = False
-    DEFAULT_HOST = 'http://alterprice.staging.corp.createdigital.me'
+
     MEDIA_ROOT = '/media'
     STATIC_ROOT = '/static'
+    ALLOWED_HOSTS = '*'
