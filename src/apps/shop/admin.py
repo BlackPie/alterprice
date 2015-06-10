ec7473424faa6a91fb71ffe7ece4da9d5db58cfd
Nikita Kolesnikov <koles.web@gmail.com>
2015-06-10 18:56:47 +0300

    OperatorFilter added for ShopAdmin

--- src/apps/shop/admin.py
+++ src/apps/shop/admin.py
@@ -1,9 +1,29 @@
+from django.db.models import Q
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 # Project imports
 from shop import models
 from utils.admin_filters import RegDateFilter
 User = get_user_model()
+
+
+class OperatorFilter(admin.SimpleListFilter):
+    title = _('По личному оператору')
+    parameter_name = 'personal_operator'
+
+    def lookups(self, request, model_admin):
+        operators = User.objects.filter(user_type=User.OPERATOR)
+        lkps = list()
+        lkps.append(('asd', 'asd'))
+        if operators.exists():
+            for o in operators:
+                lkps.append((o.id, o.email))
+        return lkps
+
+    def queryset(self, request, queryset):
+        return queryset
+
 
 
 class ShopYMLInline(admin.StackedInline):
@@ -13,11 +33,22 @@ class ShopYMLInline(admin.StackedInline):
 class ShopAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'user', 'approved')
     inlines = [ShopYMLInline]
-    list_filter = (RegDateFilter,)
+    list_filter = (RegDateFilter, OperatorFilter)
 
     def render_change_form(self, request, context, *args, **kwargs):
         user_qs = User.objects.get_list(client=True)
         context['adminform'].form.fields['user'].queryset = user_qs
         return super(ShopAdmin, self).render_change_form(request, context, args, kwargs)
+
+    def queryset(self, request):
+        qs = super(ShopAdmin, self).queryset(request)
+        if request.user.is_operator():
+            # TODO: test
+            qs = qs.filter(
+                Q(user__clientprofile__operator=request.user) |
+                Q(user__clientprofile__operator__isnull=True)
+            )
+        return qs
+
 
 admin.site.register(models.Shop, ShopAdmin)
