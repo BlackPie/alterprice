6a74f63e283707f9eff9199fb47bb22c3c78fa78
poltor <v.poltoratsky@gmail.com>
2015-06-30 19:26:34 +0300

    Statistics

--- src/apps/client/urls.py
+++ src/apps/client/urls.py
@@ -29,6 +29,7 @@ urlpatterns = patterns(
     url(r'^shop/(?P<pk>\d+)/$', views.ClientShopDetailPageView.as_view(), name='shop_detail'),
     url(r'^wallet/balance/$', views.ClientWalletBalancePageView.as_view(), name='wallet_balance'),
     url(r'^wallet/refill/$', views.ClientWalletRefillPageView.as_view(), name='wallet_refill'),
+    url(r'^statistics/$', views.ClientStatisticsDetailPageView.as_view(), name='statistics_detail'),
     url(r'^pricelist/(?P<pk>\d+)/$', views.ClientPricelistDetailPageView.as_view(), name='pricelist_detail'),
 
     # Middleware UL
