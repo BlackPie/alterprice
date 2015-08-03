15367f0e2e77d0506ece6b155f1407472ee719dd
Vladimir Shulyak <vladimir@shulyak.net>
2015-08-03 15:55:24 +0300

    Added production settings

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,3 +1,4 @@
 from configurations import Configuration # NOQA
 from .development import Development # NOQA
 from .staging import Staging # NOQA
+from .production import Production # NOQA
---
+++ src/settings/production.py
@@ -0,0 +1,13 @@
+from .base import Base
+import djcelery
+
+
+djcelery.setup_loader()
+
+
+class Production(Base):
+    DEBUG = False
+
+    MEDIA_ROOT = '/media'
+    STATIC_ROOT = '/static'
+    ALLOWED_HOSTS = '*'
