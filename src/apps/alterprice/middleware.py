e873fb07c6ffe420d89c06e77b7815afcb38f422
Dmitry Fedotkin <fedotkin.dmitry@gmail.com>
2015-08-19 15:09:36 +0500

    IpRange.DoesNotExist handler added

--- src/apps/alterprice/middleware.py
+++ src/apps/alterprice/middleware.py
@@ -11,14 +11,21 @@ class CityMiddleware(object):
     def process_request(self, request):
         if 'city_id' not in request.session.keys():
             ip = get_ip(request)
-            city_name = IpRange.objects.by_ip(ip).city.name
 
-            if city_name not in (MSC, SP):
-                city_id = City.objects.get(default_city=True).id
+            try:
+                city_name = IpRange.objects.by_ip(ip).city.name
+            except IpRange.DoesNotExist:
+                city_name = None
+
+            if city_name:
+                if city_name not in (MSC, SP):
+                    city_id = City.objects.get(default_city=True).id
+                else:
+                    try:
+                        city_id = City.objects.get(name=city_name).id
+                    except City.DoesNotExist:
+                        city_id = None
             else:
-                try:
-                    city_id = City.objects.get(name=city_name).id
-                except City.DoesNotExist:
-                    city_id = None
+                city_id = None
 
             request.session['city_id'] = city_id
